deef1fd7cf3e5ad370351ac4bb3d60f2d0c12163
VishalSingh-07 <vishalsinghagr25@gmail.com>
2022-07-23 20:23:11 +0530

    Second Part: Adding props, map and Arrow Function in a code

--- src/Components/APP.jsx
+++ src/Components/APP.jsx
@@ -1,14 +1,17 @@
 import React from "react"
-import ReactDOM from "react-dom/client";
 import Header from "./Header"
 import Footer from "./Footer"
 import Note from "./Note"
+import notes from "../notes"
+
 function APP() {
 	return (
 		<div>
 			<Header />
-                  <Footer />
-                  <Note />
+			<Footer />
+			{notes.map((notes) =>
+					<Note key={notes.key} title={notes.title} content={notes.content} />
+			)}
 		</div>
 	)
 }
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import ReactDOM from "react-dom/client"
 
 function Footer() {
 	var currentTime = new Date()
